ddf7829f660faecb05d1b0aaf7e2684a95f8e163
Austin <austinzhang1018@gmail.com>
2025-05-19 18:35:24 +0800

    added cloid support to modify request

--- src/exchange/modify.rs
+++ src/exchange/modify.rs
@@ -1,14 +1,38 @@
-use super::{order::OrderRequest, ClientOrderRequest};
-use serde::{Deserialize, Serialize};
+use crate::helpers::uuid_to_hex_string;
 
+use super::{ order::OrderRequest, ClientOrderRequest };
+use serde::{ Deserialize, Serialize };
+use uuid::Uuid;
+
+#[derive(Debug, Clone)]
+pub enum ClientId {
+    Oid(u64),
+    Cloid(Uuid),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
+pub enum SerializedClientId {
+    U64(u64),
+    Str(String),
+}
+
+impl From<ClientId> for SerializedClientId {
+    fn from(client_id: ClientId) -> Self {
+        match client_id {
+            ClientId::Oid(oid) => SerializedClientId::U64(oid),
+            ClientId::Cloid(cloid) => SerializedClientId::Str(uuid_to_hex_string(cloid)),
+        }
+    }
+}
 #[derive(Debug)]
 pub struct ClientModifyRequest {
-    pub oid: u64,
+    pub oid: ClientId,
     pub order: ClientOrderRequest,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ModifyRequest {
-    pub oid: u64,
+    pub oid: SerializedClientId,
     pub order: OrderRequest,
 }
